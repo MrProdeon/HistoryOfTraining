64b88b168c8a3b605e3f0be29eeb5163abf07700
Prodeon <prodeon21gmail.com>
2025-06-02 17:53:31 +0300

    This view is read-only

--- Test/test2.py
+++ Test/test2.py
@@ -1,3 +1,12 @@
-s = input()
-for i in s:
-    print(i)
+k = int(input())
+
+
+def counter_add(k):
+    def add_5():
+        return k + 5
+    return add_5
+
+
+cnt = counter_add(k)
+print(cnt())
+
